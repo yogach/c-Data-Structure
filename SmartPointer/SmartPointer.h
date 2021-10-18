4ee5e23e28bc5e45fc2d601e5d0fbaf9b0bb2520
caoyk <caoyk@nushinetech.com>
2021-10-18 14:36:24 +0800

    提交智能指针类

--- SmartPointer/SmartPointer.h
+++ SmartPointer/SmartPointer.h
@@ -1,6 +1,69 @@
 #ifndef SMARTPOINTER_H
 #define SMARTPOINTER_H
 
+namespace DTLib {
 
+template <typename T>
+class SmartPointer
+{
+protected:
+    T* m_pointer;
+
+public:
+    SmartPointer(T* p = NULL)
+    {
+        m_pointer = p;
+    }
+
+    //拷贝构造函数
+    //得到对应的内存地址后 将赋值的对象设为NULL
+    SmartPointer(const SmartPointer<T>& obj)
+    {
+        m_pointer = obj.m_pointer;
+
+        const_cast<SmartPointer<T>&>(obj).m_pointer = NULL; //使用const_cast取消对象的只读属性
+    }
+
+    SmartPointer<T>& operator = (const SmartPointer<T>& obj)
+    {
+        if( this != &obj ) //防止自赋值
+        {
+            delete m_pointer;
+
+            m_pointer = obj.m_pointer;
+
+            const_cast<SmartPointer<T>&>(obj).m_pointer = NULL;
+        }
+
+        return *this;
+    }
+
+    T* operator ->()
+    {
+        return m_pointer;
+    }
+
+    T& operator *()
+    {
+        return *m_pointer;
+    }
+
+    bool isNULL()
+    {
+        return (m_pointer == NULL);
+    }
+
+    T* get()
+    {
+        return m_pointer;
+    }
+
+    ~SmartPointer()
+    {
+        delete m_pointer;
+    }
+
+};
+}
 
 #endif // SMARTPOINTER_H
--- SmartPointer/main.cpp
+++ SmartPointer/main.cpp
@@ -1,9 +1,32 @@
 #include <iostream>
+#include "SmartPointer.h"
 
 using namespace std;
+using namespace DTLib;
+
+class Test
+{
+public:
+    Test()
+    {
+        cout << "Test()" << endl;
+    }
+
+    ~Test()
+    {
+        cout << "~Test()" << endl;
+    }
+};
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    SmartPointer<Test> sp = new Test();
+    SmartPointer<Test> nsp;
+
+    nsp = sp;
+
+    cout << sp.isNULL() << endl;
+    cout << nsp.isNULL() << endl;
+
     return 0;
 }
