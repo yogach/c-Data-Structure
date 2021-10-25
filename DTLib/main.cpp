0f7949b5d2d1958659d4433d6cc0e416cf3b790a
caoyk <caoyk@nushinetech.com>
2021-10-25 09:43:14 +0800

    提交list的父类

--- DTLib/main.cpp
+++ DTLib/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "SmartPointer.h"
 #include "Exception.h"
+#include "List.h"
 
 using namespace std;
 using namespace DTLib;
@@ -9,16 +10,7 @@ using namespace DTLib;
 
 int main()
 {
-    try
-    {
-       THROW_EXCEPTION(ArithmeticException, "test");
-    }
-    catch(const ArithmeticException e)
-    {
-        cout << "const ArithmeticException e" << endl;
-        cout << e.message() << endl;
-        cout << e.location() << endl;
-    }
+    List<int> * l = NULL;
 
     return 0;
 }
