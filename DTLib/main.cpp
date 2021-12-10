6a64c3b0aa4dcd6bf60aec760a8cddefc880354e
caoyk <caoyk@nushinetech.com>
2021-12-10 16:14:00 +0800

    提交QueueToStack实验

--- DTLib/main.cpp
+++ DTLib/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include "LinkStack.h"
 #include "Queue.h"
+#include "LinkQueue.h"
+
 
 using namespace std;
 using namespace DTLib;
@@ -12,7 +14,7 @@ protected:
     mutable LinkStack<T> m_stack_in;
     mutable LinkStack<T> m_stack_out;
 
-    void move() const
+    void move() const  //O(n)
     {
         if( m_stack_out.size() == 0 )
         {
@@ -25,12 +27,12 @@ protected:
     }
 
 public:
-    void add(const T& e)
+    void add(const T& e)  //O(1)
     {
         m_stack_in.push(e);
     }
 
-    void remove()
+    void remove()  //O(n)
     {
         move();
 
@@ -45,7 +47,7 @@ public:
 
     }
 
-    T front() const
+    T front() const //O(n)
     {
         move();
 
@@ -59,32 +61,117 @@ public:
         }
     }
 
-    void clear()
+    void clear() //O(n)
     {
         m_stack_in.clear();
         m_stack_out.clear();
     }
 
-    int length() const
+    int length() const //O(1)
     {
         return m_stack_in.size() + m_stack_out.size();
     }
 };
 
+template < typename T >
+class QueueToStack : public Stack<T>
+{
+protected:
+    LinkQueue<T> m_queue_1;
+    LinkQueue<T> m_queue_2;
+    LinkQueue<T>* m_pIn;
+    LinkQueue<T>* m_pOut;
+
+    void move() const  //O(n)
+    {
+        //将m_pIn中的元素移到m_pOut中 只剩一个元素
+        int n = m_pIn->length() - 1;
+
+        for(int i=0; i<n; i++)
+        {
+            m_pOut->add( m_pIn->front() );
+            m_pIn->remove();
+        }
+
+    }
+
+    void swap() //O(1)
+    {
+        LinkQueue<T>* tmp = m_pIn;
+
+        m_pIn = m_pOut;
+        m_pOut = tmp;
+    }
+
+public:
+    QueueToStack() //O(1)
+    {
+        m_pIn = &m_queue_1;
+        m_pOut = &m_queue_2;
+    }
+
+    void push(const T& e) //O(1)
+    {
+       m_pIn->add(e);
+    }
+
+    void pop() //O(n)
+    {
+       if(m_pIn->length() > 0)
+       {
+           move();
+
+           m_pIn->remove();
+
+           swap();
+       }
+       else
+       {
+           THROW_EXCEPTION(InvaildOperationException, "No element in current stack...");
+       }
+    }
+
+    T top() const //O(n)
+    {
+        if(m_pIn->length() > 0)
+        {
+            move();
+
+            return m_pIn->front();
+        }
+        else
+        {
+            THROW_EXCEPTION(InvaildOperationException, "No element in current stack...");
+        }
+    }
+
+    void clear() //O(n)
+    {
+        m_queue_1.clear();
+        m_queue_2.clear();
+    }
+
+    int size() const  //O(1)
+    {
+        return m_queue_1.length() + m_queue_2.length();
+    }
+
+};
+
 int main()
 {
-    StackToQueue<int> sq;
+    QueueToStack<int> qs;
 
     for(int i=0; i<20; i++)
     {
-        sq.add(i);
+        qs.push(i);
     }
 
-    while( sq.length() > 0)
+    while( qs.size() > 0)
     {
-        cout << sq.front() << endl;
+        cout << qs.top() << endl;
 
-        sq.remove();
+        qs.pop();
     }
 
     return 0;
