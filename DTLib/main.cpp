6f85144c14a1fb4e06cf5b24f664ce08a7c9fc5e
caoyk <caoyk@nushinetech.com>
2021-12-14 09:44:25 +0800

    提交String的测试代码

--- DTLib/main.cpp
+++ DTLib/main.cpp
@@ -1,178 +1,51 @@
 #include <iostream>
-#include "LinkStack.h"
-#include "Queue.h"
-#include "LinkQueue.h"
-
-
+#include "DTString.h"
 using namespace std;
 using namespace DTLib;
 
-template < typename T >
-class StackToQueue : public Queue<T>
+void test_1()
 {
-protected:
-    mutable LinkStack<T> m_stack_in;
-    mutable LinkStack<T> m_stack_out;
+    cout << "test()_1 begin..." << endl;
 
-    void move() const  //O(n)
-    {
-        if( m_stack_out.size() == 0 )
-        {
-            while( m_stack_in.size() > 0)
-            {
-                m_stack_out.push( m_stack_in.top() );
-                m_stack_in.pop();
-            }
-        }
-    }
+    String s;
 
-public:
-    void add(const T& e)  //O(1)
-    {
-        m_stack_in.push(e);
-    }
+    s = 'D';
 
-    void remove()  //O(n)
-    {
-        move();
+    cout << s.str() << endl;
+    cout << s.length() << endl;
+    cout << (s == "D") << endl;
+    cout << (s > "CCC") << endl;
 
-        if( m_stack_out.size() != 0 )
-        {
-            m_stack_out.pop();
-        }
-        else
-        {
-            THROW_EXCEPTION(InvaildOperationException, "No element in current queue...");
-        }
+    s += " this is another ";
+    cout << s.str() << endl;
+    cout << s.length() << endl;
 
-    }
+    cout << "test()_1 end..." << endl;
+}
 
-    T front() const //O(n)
-    {
-        move();
-
-        if( m_stack_out.size() != 0 )
-        {
-            return m_stack_out.top();
-        }
-        else
-        {
-            THROW_EXCEPTION(InvaildOperationException, "No element in current queue...");
-        }
-    }
-
-    void clear() //O(n)
-    {
-        m_stack_in.clear();
-        m_stack_out.clear();
-    }
-
-    int length() const //O(1)
-    {
-        return m_stack_in.size() + m_stack_out.size();
-    }
-};
-
-template < typename T >
-class QueueToStack : public Stack<T>
+void test_2()
 {
-protected:
-    LinkQueue<T> m_queue_1;
-    LinkQueue<T> m_queue_2;
-    LinkQueue<T>* m_pIn;
-    LinkQueue<T>* m_pOut;
+    cout << "test()_2 begin..." << endl;
 
-    void move() const  //O(n)
+    String a[] = {"E", "F", "G", "A", "Z"};
+    String min = a[0];
+
+    for(int i=0; i<5; i++)
     {
-        //将m_pIn中的元素移到m_pOut中 只剩一个元素
-        int n = m_pIn->length() - 1;
-
-        for(int i=0; i<n; i++)
+        if( min > a[i] )
         {
-            m_pOut->add( m_pIn->front() );
-            m_pIn->remove();
-        }
-
-    }
-
-    void swap() //O(1)
-    {
-        LinkQueue<T>* tmp = m_pIn;
-
-        m_pIn = m_pOut;
-        m_pOut = tmp;
-    }
-
-public:
-    QueueToStack() //O(1)
-    {
-        m_pIn = &m_queue_1;
-        m_pOut = &m_queue_2;
-    }
-
-    void push(const T& e) //O(1)
-    {
-       m_pIn->add(e);
-    }
-
-    void pop() //O(n)
-    {
-       if(m_pIn->length() > 0)
-       {
-           move();
-
-           m_pIn->remove();
-
-           swap();
-       }
-       else
-       {
-           THROW_EXCEPTION(InvaildOperationException, "No element in current stack...");
-       }
-    }
-
-    T top() const //O(n)
-    {
-        if(m_pIn->length() > 0)
-        {
-            move();
-
-            return m_pIn->front();
-        }
-        else
-        {
-            THROW_EXCEPTION(InvaildOperationException, "No element in current stack...");
+            min = a[i];
         }
     }
 
-    void clear() //O(n)
-    {
-        m_queue_1.clear();
-        m_queue_2.clear();
-    }
+    cout << "min = " << min.str() << endl;
 
-    int size() const  //O(1)
-    {
-        return m_queue_1.length() + m_queue_2.length();
-    }
-
-};
+    cout << "test()_2 end..." << endl;
+}
 
 int main()
-{
-    QueueToStack<int> qs;
-
-    for(int i=0; i<20; i++)
-    {
-        qs.push(i);
-    }
-
-    while( qs.size() > 0)
-    {
-        cout << qs.top() << endl;
-
-        qs.pop();
-    }
-
+{  
+    test_1();
+    test_2();
     return 0;
 }
