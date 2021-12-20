09a4f6517c77d97b42ea3ae1d2b95725d45161bb
caoyk <caoyk@nushinetech.com>
2021-12-20 10:12:13 +0800

    提交kmp算法实现

--- DTLib/main.cpp
+++ DTLib/main.cpp
@@ -1,35 +1,80 @@
 #include <iostream>
 #include "DTString.h"
+#include <cstring>
 using namespace std;
 using namespace DTLib;
 
+int* make_pmt(const char* p) //O(m)
+{
+    int len = strlen(p);
+    int* ret = static_cast<int*>(malloc(sizeof(int) * len));
+
+    if( ret != NULL )
+    {
+        int ll = 0;
+
+        ret[0] = 0; //从pmt定义可得 1个字符的ll值肯定为0
+
+        for(int i=1; i<len; i++)
+        {
+            while( (ll > 0) && (p[ll] != p[i]) )
+            {
+                ll = ret[ll - 1]; //通过递推的方式得到ll值
+            }
+
+            if( p[ll] == p[i] )
+            {
+                ll++;
+            }
+
+            ret[i] = ll;
+        }
+    }
+
+    return ret;
+}
+
+int kmp(const char* s, const char* p) //O(m) + O(n)
+{
+    int ret = -1;
+    int sl = strlen(s);
+    int pl =  strlen(p);
+    int* pmt = make_pmt(p);
+
+    if( (pmt != NULL) && (0 < pl) && (pl < sl) )
+    {
+        for(int i=0, j=0; i<sl; i++)
+        {
+            while( (j > 0) && (s[i] != p[j]) )
+            {
+                //这里实际上已经进行了缩写，原表达式为
+                //j = j-(j-LL) = LL = PMT[j-1];
+                j = pmt[j-1];
+            }
+
+            //当两者相等时 j++
+            if( s[i] == p[j] )
+            {
+                j++;
+            }
+
+            //当j等于p长度时，p就是s的子串
+            if( j == pl )
+            {
+                ret = i + 1 - pl; //返回p在s中的起始位置
+                break;
+            }
+        }
+    }
+
+    free(pmt);
+
+    return ret;
+}
 
 int main()
 {
-    String s = "this is test";
-
-    cout << s.startWith("this") << endl;
-    cout << s.startWith("a") << endl;
-    cout << s.endOf("test") << endl;
-    cout << s.endOf("ccc") << endl;
-
-
-    for(int i=0; i<s.length(); i++)
-    {
-        cout << s[i] << endl;
-    }
-
-    String s1 = "";
-
-    s1.insert(0,"insert test");
-
-    cout << s1.str() << endl;
-
-
-    String s2 = "  trim test ... ..   ";
-
-    cout << s2.trim().str() << endl;
-
+    cout << kmp("abcde", "cde") << endl;
 
     return 0;
 }
