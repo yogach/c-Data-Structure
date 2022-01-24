4dfe6c3420e0f54728c34f7aa7ae186859223d2e
caoyk <caoyk@nushinetech.com>
2022-01-24 10:17:44 +0800

    提交BTree的find操作

--- DTLib/BTree.h
+++ DTLib/BTree.h
@@ -13,6 +13,63 @@ namespace DTLib
 template < typename T >
 class BTree : public Tree<T>
 {
+protected:
+    virtual BTreeNode<T>* find(BTreeNode<T>* node, const T& value) const
+    {
+        BTreeNode<T>* ret = NULL;
+
+        if( node != NULL )
+        {
+            //如果当前节点的值等于需要的值 直接返回
+            if( node->value == value )
+            {
+                ret = node;
+            }
+            else
+            {
+                //如果返回值为空 则到左右子节点内进行递归查找
+                if( ret == NULL )
+                {
+                    ret = find(node->left, value);
+                }
+
+                if( ret == NULL )
+                {
+                    ret = find(node->right, value);
+                }
+            }
+        }
+
+        return ret;
+    }
+
+    virtual BTreeNode<T>* find(BTreeNode<T>* node, BTreeNode<T>* obj) const
+    {
+        BTreeNode<T>* ret = NULL;
+
+        if( node == obj )
+        {
+            ret = node;
+        }
+        else
+        {
+            if( node != NULL )
+            {
+                //如果返回值为空 则到左右子节点内进行递归查找
+                if( ret == NULL )
+                {
+                    ret = find(node->left, obj);
+                }
+
+                if( ret == NULL )
+                {
+                    ret = find(node->right, obj);
+                }
+            }
+        }
+
+        return ret;
+    }
 public:
     bool insert(TreeNode<T>* node)
     {
@@ -38,14 +95,14 @@ public:
         return NULL;
     }
 
-    BTreeNode<T>* find(const T& value)
+    BTreeNode<T>* find(const T& value) const
     {
-        return NULL;
+        return find(root(), value);
     }
 
     BTreeNode<T>* find(TreeNode<T>* node) const
     {
-        return NULL;
+        return find(root(), dynamic_cast<BTreeNode<T>*>(node));
     }
 
     BTreeNode<T>* root() const
--- DTLib/main.cpp
+++ DTLib/main.cpp
@@ -1,72 +1,17 @@
 #include <iostream>
 #include "GTree.h"
+#include "BTree.h"
 
 using namespace std;
 using namespace DTLib;
 
 int main()
-{
-    GTree<char> t;
-    GTreeNode<char>* node = NULL;
-    GTreeNode<char> tmp;
+{    
+    BTree<int> bt;
+    BTreeNode<int> btn;
 
-    tmp.value = 'A';
-    tmp.parent = NULL;
-    //t.insert('A', NULL);
-    t.insert(&tmp);
-
-    node = t.find('A');
-    t.insert('B', node);
-    t.insert('C', node);
-    t.insert('D', node);
-
-    node = t.find('B');
-    t.insert('E', node);
-    t.insert('F', node);
-
-    node = t.find('E');
-    t.insert('K', node);
-    t.insert('L', node);
-
-    node = t.find('C');
-    t.insert('G', node);
-
-    node = t.find('D');
-    t.insert('H', node);
-    t.insert('I', node);
-    t.insert('J', node);
-
-
-    node = t.find('H');
-    t.insert('M', node);
-
-    //SharedPointer<Tree<char>> r = t.remove('D');
-
-    /*char* s = "KLFGMIJ";
-
-    for(int i=0; i<7; i++)
-    {
-        TreeNode<char>* node = r->find(s[i]);
-
-        while( node != NULL )
-        {
-            cout << node->value << " ";
-
-            node = node->parent;
-        }
-
-        cout << endl;
-    }
-    */
-
-    cout << t.count() << endl;
-    cout << t.height() << endl;
-    cout << t.degree() << endl;
-
-    for(t.begin(); !t.end(); t.next())
-    {
-        cout << t.current() << endl;
-    }
+    bt.find(1);
+    bt.find(&btn);
 
     return 0;
 }
