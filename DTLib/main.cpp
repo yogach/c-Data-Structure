f567ec5125232145a1f47e9cc0c7c66e1322a6aa
caoyk <caoyk@nushinetech.com>
2022-02-07 10:23:41 +0800

    完成二叉树的面试题分析

--- DTLib/main.cpp
+++ DTLib/main.cpp
@@ -5,74 +5,221 @@
 using namespace std;
 using namespace DTLib;
 
-int main()
-{    
-    BTree<int> bt;
-    BTreeNode<int>* n;
+template < typename T >
+BTreeNode<T>* createTree()
+{
+    static BTreeNode<int> ns[9];
 
-    bt.insert(1, NULL);
+    for(int i=0; i<9; i++)
+    {
+        ns[i].value = i;
+        ns[i].parent = NULL;
+        ns[i].left = NULL;
+        ns[i].right = NULL;
+    }
 
-    n = bt.find(1);
-    bt.insert(2, n);
-    bt.insert(3, n);
+    ns[0].left = &ns[1];
+    ns[0].right = &ns[2];
+    ns[1].parent = &ns[0];
+    ns[2].parent = &ns[0];
 
-    n = bt.find(2);
-    bt.insert(4, n);
-    bt.insert(5, n);
+    ns[1].left = &ns[3];
+    ns[1].right = NULL;
+    ns[3].parent = &ns[1];
 
-    n = bt.find(4);
-    bt.insert(8, n);
-    bt.insert(9, n);
+    ns[2].left = &ns[4];
+    ns[2].right = &ns[5];
+    ns[4].parent = &ns[2];
+    ns[5].parent = &ns[2];
 
-    n = bt.find(5);
-    bt.insert(10, n, LEFT);
+    ns[3].left = NULL;
+    ns[3].right = &ns[6];
+    ns[6].parent = &ns[3];
 
-    n = bt.find(3);
-    bt.insert(6, n);
-    bt.insert(7, n);
+    ns[4].left = &ns[7];
+    ns[4].right = NULL;
+    ns[7].parent = &ns[4];
 
-    cout << bt.count() << endl;
-    cout << bt.height() << endl;
-    cout << bt.degree() << endl;
+    ns[5].left = &ns[8];
+    ns[5].right = NULL;
+    ns[8].parent = &ns[5];
+
+    return ns;
+}
+
+template < typename T >
+void printInOrder(BTreeNode<T>* node)
+{
+    if( node != NULL )
+    {
+        printInOrder(node->left);
+
+        cout << node->value <<" ";
+
+        printInOrder(node->right);
+    }
+}
+
+template < typename T >
+void printDualList(BTreeNode<T>* node)
+{
+    BTreeNode<T>* g = node;
+
+    cout << "head -> tail: " << endl;
+
+    while( node != NULL )
+    {
+        cout << node->value << " ";
+
+        g = node;
+
+        node = node->right;
+    }
 
     cout << endl;
 
-    BTree<int> nbt;
+    cout << "tail -> head: " << endl;
 
-    nbt.insert(0, NULL);
-
-    n = nbt.find(0);
-    nbt.insert(6, n);
-    nbt.insert(2, n);
-
-    n = nbt.find(2);
-    nbt.insert(7, n);
-    nbt.insert(8, n);
-
-  /*  SharedPointer< BTree<int> > t = bt.add(nbt);
-
-    SharedPointer< Array<int> > r = t->traversal(LevelOrder);
-
-    for(int i=0; i<r->length(); i++)
+    while( g != NULL )
     {
-        cout << (*r)[i] << " ";
-    }
-*/
+        cout << g->value << " ";
 
-    BTreeNode<int>* tmp = bt.thread(LevelOrder);
-
-    BTreeNode<int>* head = tmp;
-
-    while( head->right != NULL )
-    {
-        head = head->right;
+        g = g->left;
     }
 
-    while (head != NULL)
+    cout << endl;
+}
+
+template < typename T >
+void inOrderThread(BTreeNode<T>* node, BTreeNode<T>*& pre)
+{
+    if( node != NULL )
     {
-        cout << head->value << " ";
-        head = head->left;
+        inOrderThread(node->left, pre);
+
+        //使用pre指针作为一个辅助指针
+        node->left = pre;
+
+        if( pre != NULL )
+        {
+           pre->right = node;
+        }
+
+        pre = node;
+
+        inOrderThread(node->right, pre);
     }
+}
+
+template < typename T >
+BTreeNode<T>* inOrderThread1(BTreeNode<T>* node)
+{
+    BTreeNode<T>* pre = NULL;
+
+    inOrderThread(node, pre);
+
+    //将node指针重新设置回left
+    while( (node != NULL) && (node->left != NULL ))
+    {
+        node = node->left;
+    }
+
+    return node;
+}
+
+//删除单度节点
+template < typename T>
+void delOdd2(BTreeNode<T>*& node)
+{
+    if( node != NULL )
+    {
+        if( ((node->left != NULL) && (node->right == NULL)) ||
+            ((node->left == NULL) && (node->right != NULL)) )
+        {
+            BTreeNode<T>* node_child = (node->left != NULL) ? node->left : node->right;
+
+            if( node->flag() )
+            {
+                delete node;
+            }
+
+            node = node_child;
+
+            delOdd2(node);
+        }
+        else
+        {
+            delOdd2(node->left);
+            delOdd2(node->right);
+        }
+
+    }
+}
+
+//删除单度节点
+template < typename T>
+BTreeNode<T>* delOdd1(BTreeNode<T>* node)
+{
+    BTreeNode<T>* ret = NULL;
+
+    if( node != NULL )
+    {
+        if( ((node->left != NULL) && (node->right == NULL)) ||
+            ((node->left == NULL) && (node->right != NULL)) )
+        {
+            BTreeNode<T>* parent = dynamic_cast<BTreeNode<T>*>(node->parent);
+            BTreeNode<T>* node_child = (node->left != NULL) ? node->left : node->right;
+
+            if( parent != NULL )
+            {
+                //得到当前节点是父节点的左节点还是右节点
+                BTreeNode<T>*& parent_child = (parent->left == node) ? parent->left : parent->right;
+
+                //设置节点的父子关系
+                parent_child = node_child;
+                node_child->parent = parent;
+            }
+            else
+            {
+                node_child->parent = NULL; //如果父节点为空 设置当前节点的子节点为根节点
+            }
+
+            if( node->flag() )
+            {
+                delete node;
+            }
+
+            ret = delOdd1(node_child); //继续递归 删除节点
+        }
+        else
+        {
+            delOdd1(node->left);
+            delOdd1(node->right);
+
+            ret = node; //这种情况下不删除节点
+        }
+    }
+
+    return ret;
+}
+
+int main()
+{
+    BTreeNode<int>* ns = createTree<int>();
+
+    printInOrder(ns);
+
+    cout << endl;
+
+    delOdd2(ns);
+
+    printInOrder(ns);
+
+    cout << endl;
+
+    ns = inOrderThread1(ns);
+
+    printDualList(ns);
 
     return 0;
 }
